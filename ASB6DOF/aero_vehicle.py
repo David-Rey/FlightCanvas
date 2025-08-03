fixed negative sign bug

ASB6DOF/aero_vehicle.py
@@ -110,7 +110,7 @@ class AeroVehicle:
 
     def run_sim(self, tf):
         # Initial state
-        pos_0 = np.array([0, 0, 350])  # Initial position
+        pos_0 = np.array([0, 0, 850])  # Initial position
         vel_0 = np.array([25, 0, -2])  # Initial velocity
         quat_0 = utils.euler_to_quat((0, 0, 0))
         omega_0 = np.array([0, 0, 0])  # Initial angular velocity
@@ -138,6 +138,7 @@ class AeroVehicle:
 
         # Gravity in the inertial frame
         g = np.array([0, 0, -9.81])
+        #g = np.array([0, 0, 0])
 
         # Forces and moments (in the body frame)
         F_B, M_B = self.compute_forces_and_moments_lookup(quat, vel_I, omega_B, self.xyz_ref)
@@ -247,8 +248,8 @@ class AeroVehicle:
         grid = pv.Plane(
             center=(0, 0, 0),  # Center of the plane
             direction=(0, 0, 1),  # Normal vector, perpendicular to the XY plane
-            i_size=1000,  # Width of the grid in X direction
-            j_size=1000,  # Height of the grid in Y direction
+            i_size=2000,  # Width of the grid in X direction
+            j_size=2000,  # Height of the grid in Y direction
             i_resolution=20,  # Number of subdivisions along X
             j_resolution=20  # Number of subdivisions along Y
         )

ASB6DOF/components/aero_component.py
@@ -104,8 +104,9 @@ class AeroComponent(ABC):
             M_b[2] = -M_b[2]
 
         lever_arm = self.xyz_ref - com
-        M_b_cross = np.cross(lever_arm, F_b)
+        M_b_cross = np.cross(-lever_arm, F_b)
         M_b_total = M_b + M_b_cross
+        #M_b_total = M_b_cross
         return F_b, M_b_total
 
     def init_buildup_manager(self, vehicle_path):

ASB6DOF/main.py
@@ -76,12 +76,12 @@ if __name__ == '__main__':
 
     aero_vehicle = AeroVehicle(
         name="David's Glider",
-        xyz_ref=[0.05, 0, 0],  # Vehicle's Center of Gravity
+        xyz_ref=[0.15, 0, 0],  # Vehicle's Center of Gravity
         components=all_components
     )
 
-    #aero_vehicle.compute_buildup()
-    #aero_vehicle.save_buildup()
+    aero_vehicle.compute_buildup()
+    aero_vehicle.save_buildup()
     #aero_vehicle.save_buildup_fig()
     aero_vehicle.load_buildup()
     #aero_vehicle.compute_forces_and_moments_lookup(quat, vel, omega)
@@ -90,19 +90,12 @@ if __name__ == '__main__':
     #                         np.deg2rad([0, 30]))
     #aero_vehicle.test_remove_me()
 
-    t_arr, x_arr = aero_vehicle.run_sim(20)
+    t_arr, x_arr = aero_vehicle.run_sim(40)
 
-    '''
-    plt.figure()
-    plt.plot(t_arr, x_arr[2])
-
-    plt.figure()
-    plt.plot(t_arr, x_arr[-2])
-    plt.show()
-    '''
 
     # Visualize the Vehicle
     aero_vehicle.init_actors(color='lightblue', show_edges=False, opacity=1)
+    #aero_vehicle.init_debug()
     #aero_vehicle.init_debug()
 
     aero_vehicle.animate(t_arr, x_arr)